e4ba0f72b6c2ad8a18207f11f2e324f26a351168
paul-hyun <cchyun@gmail.com>
2023-07-19 15:13:12 +0900

    add urdf_tutorial

---
+++ ros_ws/src/urdf_tutorial/launch/robot_1.launch.py
@@ -0,0 +1,29 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    pkg_path = os.path.join(get_package_share_directory("urdf_tutorial"))
+    xacro_file = os.path.join(pkg_path, "urdf", "robot_1.xacro")
+    robot_description = xacro.process_file(xacro_file)
+    params = {"robot_description": robot_description.toxml(), "use_sim_time": use_sim_time}
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time", default_value="false", description="use sim time"
+            ),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                parameters=[params],
+            ),
+        ]
+    )
---
+++ ros_ws/src/urdf_tutorial/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'urdf_tutorial'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cchyun',
+    maintainer_email='cchyun@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
