15a13a995efb8b3d4c3addf15d4177cf40da4b15
paul-hyun <cchyun@gmail.com>
2023-08-19 14:36:15 +0900

    update car_tutorial

--- ros_ws/src/car_odom/src/car_odom.cpp
+++ ros_ws/src/car_odom/src/car_odom.cpp
@@ -1,7 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
-#include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 
 
@@ -11,7 +11,7 @@ using std::placeholders::_1;
 class CarOdom:public rclcpp::Node
 {
     private:
-        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_vel_raw;
+        rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr sub_vel_raw;
         std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
 
         double scale_x = 1.0;
@@ -29,7 +29,7 @@ class CarOdom:public rclcpp::Node
         double pos_y = 0.0;
         double pos_z = 0.0;
 
-        rclcpp::Time last_time = this->get_clock()->now();
+        rclcpp::Time last_time;
         geometry_msgs::msg::TransformStamped tf;
 
     public:
@@ -46,19 +46,23 @@ class CarOdom:public rclcpp::Node
             tf.child_frame_id = "base_link";
 
             tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-            sub_vel_raw = this->create_subscription<geometry_msgs::msg::Twist>("vel_raw", 10, std::bind(&CarOdom::vel_raw_callback, this, _1));
+            sub_vel_raw = this->create_subscription<geometry_msgs::msg::TwistStamped>("vel_raw", 10, std::bind(&CarOdom::vel_raw_callback, this, _1));
         }
     
     private:
-        void vel_raw_callback(const std::shared_ptr<geometry_msgs::msg::Twist > msg) {
+        void vel_raw_callback(const std::shared_ptr<geometry_msgs::msg::TwistStamped> msg) {
             // delta time
-            rclcpp::Time curr_time = this->get_clock()->now();
+            rclcpp::Time curr_time = msg->header.stamp;
+            if (last_time.seconds() == 0) {
+                last_time = curr_time;
+                return;
+            }
             delta_time = (curr_time - last_time).seconds();
             last_time = curr_time;
             // recv value
-            vel_x = msg->linear.x * scale_x;
-    		vel_y = msg->linear.y * scale_y;
-    		vel_z = msg->angular.z * scale_z;
+            vel_x = msg->twist.linear.x * scale_x;
+    		vel_y = msg->twist.linear.y * scale_y;
+    		vel_z = msg->twist.angular.z * scale_z;
             // delta value
             delta_x = (vel_x * cos(pos_z) - vel_y * sin(pos_z)) * delta_time;
             delta_y = (vel_x * sin(pos_z) + vel_y * cos(pos_z)) * delta_time;
--- ros_ws/src/car_tutorial/car_tutorial/fake_driver.py
+++ ros_ws/src/car_tutorial/car_tutorial/fake_driver.py
@@ -4,7 +4,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, TwistStamped
 from sensor_msgs.msg import JointState, Image
 
 import cv2
@@ -20,7 +20,7 @@ class FakeDriver(Node):
         # cmd vel subscriber
         self.sub_cmd_vel = self.create_subscription(Twist, "cmd_vel", self.cmd_vel_callback, 10)
         # vel raw publisher
-        self.pub_vel_raw = self.create_publisher(Twist, "vel_raw", 10)
+        self.pub_vel_raw = self.create_publisher(TwistStamped, "vel_raw", 10)
         # img raw publisher
         self.pub_img_raw = self.create_publisher(Image, "img_raw", 10)
 
@@ -30,7 +30,7 @@ class FakeDriver(Node):
         self.joint_states.name = ["left_wheel_joint", "right_wheel_joint"]
         self.joint_states.position = [0.0, 0.0]
 
-        self.vel_raw = Twist()
+        self.vel_raw = TwistStamped()
 
         self.bridge = CvBridge()
 
@@ -38,14 +38,17 @@ class FakeDriver(Node):
         self.timer = self.create_timer(0.1, self.publish_callback)
     
     def cmd_vel_callback(self, msg):
-        # self.get_logger().info(f'recv cmd_vel message {msg}')
-        self.vel_raw = msg
+        self.get_logger().info(f'recv cmd_vel message {msg}')
+        self.vel_raw.twist = msg
     
     def publish_callback(self):
         curr_time = self.get_clock().now()
 
         # joint states
         self.joint_states.header.stamp = curr_time.to_msg()
+
+        # vel raw
+        self.vel_raw.header.stamp = curr_time.to_msg()
 
         # image
         image = cv2.imread('src/car_tutorial/car_tutorial/photo.png', cv2.IMREAD_COLOR)
